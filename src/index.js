fb2fa2d1bedd6c3f42b04a575daf405882ebe838
Suizanna <73129581+Suizanna@users.noreply.github.com>
2022-01-04 05:27:21 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
-module.exports = function reverse (n) {
-    string = Math.abs(n).toString().split('').reverse().join('');
-    return Number.parseInt(string,[Number]);
-}
+module.exports = function reverseInt(n) {
+    return parseFloat(n.toString().split("").reverse().join("")) * Math.sign(n);
+};
+
+// function reverse (n) {
+//     string = Math.abs(n).toString().split('').reverse().join('');
+//     return Number.parseInt(string,[Number]);
+// }
